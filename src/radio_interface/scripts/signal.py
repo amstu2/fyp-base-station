1c75733b040998c8f607f259bf0f2ade49bf87e9
Andrew Stuart <94andrew.stuart@gmail.com>
2019-09-02 18:05:34 +1000

    Added ROS message for txRate

--- src/radio_interface/scripts/signal.py
+++ src/radio_interface/scripts/signal.py
@@ -6,16 +6,21 @@ from fabric import Connection
 
 def getSignalStrength():
     pub = rospy.Publisher('/radio_signal', Int16, queue_size=10)
+    pub2 = rospy.Publisher('/radio_tx_rate', Int16, queue_size=10)
+
     rospy.init_node('radio_interface', anonymous=True)
     rate = rospy.Rate(2)
     ssh_connection = Connection(host="ubnt@192.168.1.201", connect_kwargs={"password":"rover"})
     while not rospy.is_shutdown():
-        raw_output = ssh_connection.run('mca-status | grep signal', hide=True)
+        raw_output = ssh_connection.run('mca-status | grep -Ei "signal|TxRate"', hide=True)
         msg = "{0.stdout}"
         signal_string = msg.format(raw_output)
-        signal_split = signal_string.split('=')
-        signal_int = signal_split[1]
-        pub.publish(int(signal_int))
+        initial_split = signal_string.split('=')
+        signal_split = initial_split[1].split('\r')[0]
+        tx_split = initial_split[2].split('\r')[0]
+
+        pub.publish(int(signal_split))
+        pub2.publish(int(float(tx_split)))
         rate.sleep()
 
 if __name__ == '__main__':
